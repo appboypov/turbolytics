f5416c6cad18de6f3d48aab3539141695a431a1f
appboypov <info@codaveto.com>
2026-01-06 21:20:24 +0100

    Update flutter_lints to ^6.0.0 and fix warnings

--- lib/src/enums/analytics_types.dart
+++ lib/src/enums/analytics_types.dart
@@ -203,5 +203,6 @@ extension AnalyticsTypesHelpers on AnalyticsTypes {
     required String subject,
     Map<String, Object>? parameters,
   }) =>
-      Analytic(subject: subject, type: this, parameters: parameters).toCustomAnalytic;
+      Analytic(subject: subject, type: this, parameters: parameters)
+          .toCustomAnalytic;
 }
